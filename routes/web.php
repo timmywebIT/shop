8d66200726cfe0fa659a91797f7b956894b38b3a
timmy-ctrl <timmygithub@gmail.com>
2025-10-01 13:38:51 +0800

    first

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/admin', \App\Http\Controllers\Main\IndexController::class);
+
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', \App\Http\Controllers\Category\IndexController::class)->name('category.index');
+    Route::get('/create', \App\Http\Controllers\Category\CreateController::class)->name('category.create');
+    Route::post('/', \App\Http\Controllers\Category\StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', \App\Http\Controllers\Category\EditController::class)->name('category.edit');
+    Route::get('/{category}', \App\Http\Controllers\Category\ShowController::class)->name('category.show');
+    Route::patch('/{category}', \App\Http\Controllers\Category\UpdateController::class)->name('category.update');
+    Route::delete('/{category}', \App\Http\Controllers\Category\DeleteController::class)->name('category.delete');
+});
